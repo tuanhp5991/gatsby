414648dff046385a375f1dbc8e2aab6b6b0814f6
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-09-25 11:00:32 +0200

    tmp: use local gatsby-dev-cli and not published one

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+set -e # bail on errors
+
 SRC_PATH=$1
 CUSTOM_COMMAND="${2:-yarn test}"
-GATSBY_PATH="${CIRCLE_WORKING_DIRECTORY:-../../}"
+GATSBY_PATH="${CIRCLE_WORKING_DIRECTORY:-../..}"
 
 # cypress docker does not support sudo and does not need it, but the default node executor does
-command -v gatsby-dev || command -v sudo && sudo npm install -g gatsby-dev-cli || npm install -g gatsby-dev-cli &&
+# command -v gatsby-dev || command -v sudo && sudo npm install -g gatsby-dev-cli || npm install -g gatsby-dev-cli &&
 
 # setting up child integration test link to gatsby packages
 cd "$SRC_PATH" &&
-gatsby-dev --set-path-to-repo "$GATSBY_PATH" &&
-gatsby-dev --force-install --scan-once  && # install _all_ files in gatsby/packages
-chmod +x ./node_modules/.bin/gatsby && # this is sometimes necessary to ensure executable
+eval node "$GATSBY_PATH/packages/gatsby-dev-cli/dist/index.js" --set-path-to-repo "$GATSBY_PATH" &&
+eval node "$GATSBY_PATH/packages/gatsby-dev-cli/dist/index.js" --force-install --scan-once  && # install _all_ files in gatsby/packages
+((test -f  ./node_modules/.bin/gatsby && chmod +x ./node_modules/.bin/gatsby && echo "Gatsby bin chmoded") || echo "Gatsby bin doesn't exist. Skipping chmod.") && # this is sometimes necessary to ensure executable
 sh -c "$CUSTOM_COMMAND" &&
 echo "e2e test run succeeded"
